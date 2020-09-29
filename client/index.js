96cc1292c1e1679658fc5151c34cd13bd8d11131
hugo <hugo@MacBook-Pro-de-hugo.local>
2020-09-29 03:43:57 -0300

    Inicial

---
+++ client/index.js
@@ -0,0 +1,29 @@
+'use strict'
+ 
+const Telnet = require('telnet-client')
+ 
+async function run() {
+  let connection = new Telnet()
+ 
+  // these parameters are just examples and most probably won't work for your use-case.
+  let params = {
+    host: '127.0.0.1',
+    port: 23,
+      shellPrompt: '/ # ', // or negotiationMandatory: false
+      negotiationMandatory: false,
+    timeout: 1500
+  }
+ 
+  try {
+    await connection.connect(params)
+  } catch (error) {
+      console.error("👀")
+    // handle the throw (timeout)
+      throw new Error(error)
+  }
+ 
+  let res = await connection.send('data').catch(a=>console.error(a))
+  console.log('async result:', res)
+}
+ 
+run()
