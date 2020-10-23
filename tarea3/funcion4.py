17c27c634b1e3a06367a308c10c552afaa0f184a
hugo <hugojerez21@gmail.com>
2020-10-23 08:44:20 -0300

    -a

--- tarea3/funcion4.py
+++ tarea3/funcion4.py
@@ -4,8 +4,6 @@ def filter_icmp_packets(packet):
   # Se obtiene el argumento packet
   if packet["TCP"]["flags"] == "24":
     packet["TCP"]["flags"]  = "32"
-  else:
-    print("Paquete ignorado")
   # If the condition is meet
   return packet
 
--- tarea3/funcion5.py
+++ tarea3/funcion5.py
@@ -4,8 +4,6 @@ def function(packet):
   # Se obtiene el argumento packet
   if packet["TCP"]["len"] == "0x80":
     packet["TCP"]["len"]  = "0x0"
-  else:
-    print("Paquete ignorado")
   # If the condition is meet
   return packet
 
