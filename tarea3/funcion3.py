ab40bc0c39c5a40daa7d02ea121467e326be0b38
hugo <hugojerez21@gmail.com>
2020-10-23 08:33:19 -0300

    -a

--- tarea3/funcion3.py
+++ tarea3/funcion3.py
@@ -2,8 +2,8 @@
 
 def function(packet):
   # Se obtiene el argumento packet
-  if packet["IP"]["addr"] == "127.0.0.1"
-    packet["IP"]["addr"]  = "53.123.123.222"
+  if packet["IP"]["srcport"] == "23"
+    packet["IP"]["srcport"]  = "25"
   else
     print("Paquete ignorado")
   # If the condition is meet
